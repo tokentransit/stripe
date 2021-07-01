34215d85fd9648de24867e2d8170ea5675a811e6
Morgan Conbere <mconbere@gmail.com>
2021-06-30 22:56:02 -0700

    Add listeners and responders for payment changes to get Android payment working

--- android/src/main/java/ca/zyra/capacitor/stripe/StripePlugin.kt
+++ android/src/main/java/ca/zyra/capacitor/stripe/StripePlugin.kt
@@ -1,15 +1,21 @@
 package ca.zyra.capacitor.stripe
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import androidx.activity.ComponentActivity
 import com.getcapacitor.*
 import com.stripe.android.*
 import com.stripe.android.view.BillingAddressFields
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import com.stripe.android.Stripe as StripeInstance
 
 
 @NativePlugin(requestCodes = [9972, 50000, 50001, 6000])
-class StripePlugin : Plugin(), EphemeralKeyProvider, PaymentSession.PaymentSessionListener {
-    private lateinit var stripeInstance: Stripe
+class Stripe : Plugin(), EphemeralKeyProvider, PaymentSession.PaymentSessionListener {
+    private lateinit var stripeInstance: StripeInstance
     private lateinit var publishableKey: String
     private var isTest = true
     private var customerSession: CustomerSession? = null
@@ -25,8 +31,8 @@ class StripePlugin : Plugin(), EphemeralKeyProvider, PaymentSession.PaymentSessi
 
     private var requestPaymentCallback: PluginCall? = null
 
-    private var paymentConfiguration = PaymentConfiguration()
-    private data class PaymentConfiguration(
+    private var paymentConfig = PaymentConfig()
+    private data class PaymentConfig(
         var googlePayEnabled: Boolean = false,
         var fpxEnabled: Boolean = false,
         var requiredBillingAddressFields: BillingAddressFields = BillingAddressFields.Full,
@@ -45,187 +51,376 @@ class StripePlugin : Plugin(), EphemeralKeyProvider, PaymentSession.PaymentSessi
         }
     }
 
-    @PluginMethod
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     fun setPublishableKey(call: PluginCall) {
-        try {
-            val key = call.getString("key")
+        GlobalScope.async(context = Dispatchers.Main) {
+            try {
+                Log.i(TAG, "setPublishableKey")
 
-            if (key == null || key == "") {
-                call.error("you must provide a valid key")
-                return
+                val key = call.getString("key")
+
+                if (key == null || key == "") {
+                    call.error("you must provide a valid key")
+                    return@async
+                }
+
+                stripeInstance = StripeInstance(context, key)
+                publishableKey = key
+                isTest = key.contains("test")
+                call.success()
+            } catch (e: Exception) {
+                call.error("unable to set publishable key: ${e.localizedMessage}", e)
             }
-
-            stripeInstance = Stripe(context, key)
-            publishableKey = key
-            isTest = key.contains("test")
-            call.success()
-        } catch (e: Exception) {
-            call.error("unable to set publishable key: " + e.localizedMessage, e)
         }
     }
 
     fun resetCustomerContext(context: Context): CustomerSession? {
+        Log.i(TAG, "resetCustomerContext")
+
+        PaymentConfiguration.init(context, publishableKey)
+
         CustomerSession.endCustomerSession()
         CustomerSession.initCustomerSession(context, this, true)
         customerSession = CustomerSession.getInstance()
+
         return customerSession
     }
 
+    // Implements EphemeralKeyProvider.
     override fun createEphemeralKey(apiVersion: String, keyUpdateListener: EphemeralKeyUpdateListener) {
-        val callback = customerKeyCallback ?: return
-        val callbackId = callback.data.get("callbackId") ?: return
+        GlobalScope.async(context = Dispatchers.Main) {
+            Log.i(TAG, "createEphemeralKey: apiVersion $apiVersion")
 
-        customerKeyListener = keyUpdateListener
+            val callback = customerKeyCallback ?: run {
+                Log.w(TAG, "customerKeyCallback is null in createEphemeralKey")
+                keyUpdateListener.onKeyUpdateFailure(400, "no customerKeyCallback configured")
+                return@async
+            }
 
-        val result = JSObject()
-        result.put("apiVersion", apiVersion)
-        result.put("callbackId", callbackId)
+            customerKeyListener = keyUpdateListener
 
-        callback.resolve(result)
-    }
+            val result = JSObject()
+            result.put("apiVersion", apiVersion)
+            result.put("callbackId", callback.callbackId)
 
-    @PluginMethod
-    fun setCustomerKeyCallback(call: PluginCall) {
-        customerKeyCallback?.let {
-            bridge.releaseCall(it)
+            callback.resolve(result)
         }
-
-        call.save()
-        customerKeyCallback = call
     }
 
-    @PluginMethod
-    fun setStripeAccount(call: PluginCall) {
-        call.success()
-    }
+    @PluginMethod(returnType = PluginMethod.RETURN_CALLBACK)
+    fun setCustomerKeyCallback(call: PluginCall) {
+        GlobalScope.async(context = Dispatchers.Main) {
+            try {
+                Log.i(TAG, "setCustomerKeyCallback")
 
-    @PluginMethod
-    fun customerKeyCompleted(call: PluginCall) {
-        val callbackId = call.data.get("callbackId") ?: return
-        val callback = customerKeyCallback ?: return
-
-        // customerKeyListener should be called with error for missing values callbackId, response, and if it contains error.
-
-        customerKeyListener?.onKeyUpdate(call.data.getJSObject("response").toString())
-        call.success()
-    }
-
-    @PluginMethod
-    fun setPaymentConfiguration(call: PluginCall) {
-        // ignore applePayEnabled, it doesn't make sense on Android.
-        call.data.getBool("googlePayEnabled")?.let { paymentConfiguration.googlePayEnabled = it }
-        call.data.getBool("fpxEnabled")?.let { paymentConfiguration.fpxEnabled = it }
-        call.data.getString("requiredBillingAddressFields")?.let {
-            paymentConfiguration.requiredBillingAddressFields = when (it) {
-                "none" -> BillingAddressFields.None
-                "postalCode" -> BillingAddressFields.PostalCode
-                "full" -> BillingAddressFields.Full
-                else -> {
-                    BillingAddressFields.None
+                customerKeyCallback?.let {
+                    it.release(bridge)
                 }
+
+                call.save()
+                customerKeyCallback = call
+            } catch (e: Exception) {
+                call.error("Unable to set customer key callback: ${e.localizedMessage}", e)
             }
         }
-        call.data.getString("companyName")?.let {
-            paymentConfiguration.companyName = it
-        }
-        call.data.getBool("canDeletePaymentOptions")?.let {
-            paymentConfiguration.canDeletePaymentOptions = it
-        }
-        call.data.getBool("cardScanningEnabled")?.let {
-            paymentConfiguration.cardScanningEnabled = it
-        }
-        call.success()
     }
 
-    @PluginMethod
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
+    fun customerKeyCompleted(call: PluginCall) {
+        GlobalScope.async(context = Dispatchers.Main) {
+            try {
+                Log.i(TAG, "customerKeyCompleted")
+
+                val listener = customerKeyListener ?: run {
+                    call.error("customerKeyListener must be set")
+                    return@async
+                }
+
+                val callbackId = call.data.getString("callbackId")
+                if (callbackId == null) {
+                    call.error("callbackId must not be empty")
+                    return@async
+                }
+
+                val callback = customerKeyCallback ?: run {
+                    call.error("customerKeyCallback must not be null")
+                    return@async
+                }
+
+                if (callback.callbackId != callbackId) {
+                    call.error("customerKeyCallback callbackId must be equal to callbackId")
+                    return@async
+                }
+
+                val error = call.data.getString("error")
+                if (error != null) {
+                    listener.onKeyUpdateFailure(400, error)
+                    call.success()
+                }
+
+                listener.onKeyUpdate(call.data.getJSObject("response").toString())
+                call.success()
+            } catch (e: Exception) {
+                call.error("Unable to call customer key completed: ${e.localizedMessage}", e)
+            }
+        }
+    }
+
+    @PluginMethod(returnType = PluginMethod.RETURN_PROMISE)
+    fun setPaymentConfiguration(call: PluginCall) {
+        GlobalScope.async(context = Dispatchers.Main) {
+            try {
+                Log.i(TAG, "setPaymentConfiguration")
+
+                // ignore applePayEnabled, it doesn't make sense on Android.
+                call.data.getBool("googlePayEnabled")
+                    ?.let { paymentConfig.googlePayEnabled = it }
+                call.data.getBool("fpxEnabled")?.let { paymentConfig.fpxEnabled = it }
+                call.data.getString("requiredBillingAddressFields")?.let {
+                    paymentConfig.requiredBillingAddressFields = when (it) {
+                        "none" -> BillingAddressFields.None
+                        "postalCode" -> BillingAddressFields.PostalCode
+                        "full" -> BillingAddressFields.Full
+                        else -> {
+                            BillingAddressFields.None
+                        }
+                    }
+                }
+                call.data.getString("companyName")?.let {
+                    paymentConfig.companyName = it
+                }
+                call.data.getBool("canDeletePaymentOptions")?.let {
+                    paymentConfig.canDeletePaymentOptions = it
+                }
+                call.data.getBool("cardScanningEnabled")?.let {
+                    paymentConfig.cardScanningEnabled = it
+                }
+                call.success()
+            } catch (e: Exception) {
+                call.error("Unable to set payment configuration: ${e.localizedMessage}", e)
+            }
+        }
+    }
+
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     fun updatePaymentContext(call: PluginCall) {
-        val ps = PaymentSession(activity as ComponentActivity, paymentConfiguration.config())
-        ps.init(this)
-        paymentSession = ps
+        GlobalScope.async(context = Dispatchers.Main) {
+            try {
+                Log.i(TAG, "updatePaymentContext")
+
+                val cs = customerSession ?: resetCustomerContext(context)
+
+                if (paymentSession == null) {
+                    val result =
+                        PaymentSession(activity as ComponentActivity, paymentConfig.config())
+                    result.init(this@Stripe)
+                    paymentSession = result
+                }
+
+                var amount: Long = 0
+                call.data.getLong("amount")?.let {
+                    amount = it
+                }
+
+                paymentSession?.setCartTotal(amount)
+            } catch (e: Exception) {
+                call.error("Unable to update payment context: ${e.localizedMessage}", e)
+            }
+        }
     }
 
-    @PluginMethod
+    @PluginMethod(returnType = PluginMethod.RETURN_CALLBACK)
     fun setPaymentContextFailedToLoadCallback(call: PluginCall) {
-        paymentSessionFailedToLoadCallback?.let {
-            bridge.releaseCall(it)
-        }
+        GlobalScope.async(context = Dispatchers.Main) {
+            try {
+                Log.i(TAG, "setPaymentContextFailedToLoadCallback")
 
-        call.save()
-        paymentSessionFailedToLoadCallback = call
+                paymentSessionFailedToLoadCallback?.let {
+                    bridge.releaseCall(it)
+                }
+
+                call.save()
+                paymentSessionFailedToLoadCallback = call
+            } catch (e: Exception) {
+                call.error(
+                    "Unable to set payment context failed to load callback: ${e.localizedMessage}",
+                    e
+                )
+            }
+        }
     }
 
-    @PluginMethod
+    @PluginMethod(returnType = PluginMethod.RETURN_CALLBACK)
     fun setPaymentContextCreatedPaymentResultCallback(call: PluginCall) {
-        paymentSessionCreatedPaymentResultCallback?.let {
-            bridge.releaseCall(it)
-        }
+        GlobalScope.async(context = Dispatchers.Main) {
+            try {
+                Log.i(TAG, "setPaymentContextCreatedPaymentResultCallback")
 
-        call.save()
-        paymentSessionCreatedPaymentResultCallback = call
+                paymentSessionCreatedPaymentResultCallback?.let {
+                    bridge.releaseCall(it)
+                }
+
+                call.save()
+                paymentSessionCreatedPaymentResultCallback = call
+            } catch (e: Exception) {
+                call.error(
+                    "Unable to set payment context created payment result callback: ${e.localizedMessage}",
+                    e
+                )
+            }
+        }
     }
 
-    @PluginMethod
+    @PluginMethod(returnType = PluginMethod.RETURN_CALLBACK)
     fun setPaymentContextDidChangeCallback(call: PluginCall) {
-        paymentSessionDidChangeCallback?.let {
-            bridge.releaseCall(it)
-        }
+        GlobalScope.async(context = Dispatchers.Main) {
+            try {
+                Log.i(TAG, "setPaymentContextDidChangeCallback")
 
-        call.save()
-        paymentSessionDidChangeCallback = call
+                paymentSessionDidChangeCallback?.let {
+                    bridge.releaseCall(it)
+                }
+
+                call.save()
+                paymentSessionDidChangeCallback = call
+            } catch (e: Exception) {
+                call.error(
+                    "Unable to set payment context did change callback: ${e.localizedMessage}",
+                    e
+                )
+            }
+        }
     }
 
-    @PluginMethod
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     fun requestPayment(call: PluginCall) {
-        requestPaymentCallback = call
-        val paymentMethod = currentPaymentSessionData?.paymentMethod ?: return
+        GlobalScope.async(context = Dispatchers.Main) {
+            try {
+                Log.i(TAG, "requestPayment")
 
-        paymentSessionCreatedPaymentResultCallback?.let {
-            var result = JSObject()
-            result.put("paymentMethod", paymentMethod)
-            it.success(result)
+                requestPaymentCallback = call
+                val paymentMethod = currentPaymentSessionData?.paymentMethod ?: throw Exception("No currentPaymentSessionData paymentMethod")
+
+                paymentSessionCreatedPaymentResultCallback?.let {
+                    val result = JSObject()
+                    result.put("paymentMethod", pmToJson(paymentMethod))
+                    it.success(result)
+                }
+            } catch (e: Exception) {
+                call.error("Unable to request payment: ${e.localizedMessage}", e)
+            }
         }
     }
 
-    @PluginMethod
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     fun showPaymentOptions(call: PluginCall) {
-        paymentSession?.presentPaymentMethodSelection()
-    }
+        GlobalScope.async(context = Dispatchers.Main) {
+            try {
+                Log.i(TAG, "showPaymentOptions")
 
-    @PluginMethod
-    fun currentPaymentOption(call: PluginCall) {
-        var result = JSObject()
-        currentPaymentSessionData?.let {
-            val name = it.paymentMethod?.card?.brand?.displayName
-            val last4 = it.paymentMethod?.card?.last4
+                val ps = paymentSession ?: run {
+                    Log.w(TAG, "showPaymentOptions paymentSession is null")
+                    throw Error("paymentSession is null")
+                }
 
-            result.put("label", (name ?: "") + " " + (last4 ?: ""))
+                ps.presentPaymentMethodSelection()
+
+                call.success()
+            } catch (e: Exception) {
+                call.error("Unable to show payment options: ${e.localizedMessage}", e)
+            }
         }
-
-        call.success(result)
     }
 
-    @PluginMethod
+    @PluginMethod(returnType = PluginMethod.RETURN_PROMISE)
+    fun currentPaymentOption(call: PluginCall) {
+        GlobalScope.async(context = Dispatchers.Main) {
+            try {
+                Log.i(TAG, "currentPaymentOption")
+
+                val result = JSObject()
+                currentPaymentSessionData?.let {
+                    val name = it.paymentMethod?.card?.brand?.displayName
+                    val last4 = it.paymentMethod?.card?.last4
+
+                    result.put("label", (name ?: "") + " " + (last4 ?: ""))
+
+                    // The icon resource is: it.paymentMethod?.card?.brand?.icon but we don't have a way to load this into the same format (a data url) as iOS.
+                }
+
+                call.success(result)
+            } catch (e: Exception) {
+                call.error("Unable to return current payment option: ${e.localizedMessage}", e)
+            }
+        }
+    }
+
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     fun paymentContextCreatedPaymentResultCompleted(call: PluginCall) {
+        GlobalScope.async(context = Dispatchers.Main) {
+            try {
+                Log.i(TAG, "paymentContextCreatedPaymentResultCompleted")
+
+                call.success()
+            } catch (e: Exception) {
+                call.error(
+                    "Unable to process payment context created payment result completed: ${e.localizedMessage}",
+                    e
+                )
+            }
+        }
     }
 
-    @PluginMethod
+    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     fun clearContext(call: PluginCall) {
-        customerSession = null
-        paymentSession = null
+        GlobalScope.async(context = Dispatchers.Main) {
+            try {
+                Log.i(TAG, "clearContext")
+
+                CustomerSession.endCustomerSession()
+                customerSession = null
+                paymentSession = null
+
+                call.success()
+            } catch (e: Exception) {
+                call.error("Unable to clear context: ${e.localizedMessage}", e)
+            }
+        }
     }
 
     override fun onCommunicatingStateChanged(isCommunicating: Boolean) {
+        GlobalScope.async(context = Dispatchers.Main) {
+            Log.i(TAG, "onCommunicatingStateChanged(${isCommunicating})")
+        }
     }
 
     override fun onError(errorCode: Int, errorMessage: String) {
-        paymentSessionFailedToLoadCallback?.let {
+        GlobalScope.async(context = Dispatchers.Main) {
+            Log.w(TAG, "onError($errorCode, $errorMessage)")
+            val callback = paymentSessionFailedToLoadCallback ?: run {
+                Log.w(
+                    TAG,
+                    "onError($errorCode, $errorMessage) returned but paymentSessionFailedToLoadCallback is null"
+                )
+                return@async
+            }
+
             val result = JSObject()
             result.put("error", errorMessage)
-            it.resolve(result)
+            callback.resolve(result)
         }
     }
 
     override fun onPaymentSessionDataChanged(data: PaymentSessionData) {
-        currentPaymentSessionData = data
+        GlobalScope.async(context = Dispatchers.Main) {
+            Log.i(TAG, "onPaymentSessionDataChanged")
+
+            currentPaymentSessionData = data
+        }
+    }
+
+    override fun handleOnActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.handleOnActivityResult(requestCode, resultCode, data)
+        paymentSession?.handlePaymentData(requestCode, resultCode, data)
     }
 }
